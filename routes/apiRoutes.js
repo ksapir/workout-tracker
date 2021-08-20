d8e636967ea8775f8fedc81e527d5fc2d4075744
Karen Pion <karenlpion@gmail.com>
2021-08-20 02:37:18 -0400

    fixing range

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -45,12 +45,18 @@ router.post("/api/workouts", ({ body }, res) => {
 
 // Get workouts in a range
 router.get("/api/workouts/range", (req, res) => {
-    db.Workout.find({}).limit().
-    then(dbWorkout => {
-        res.json(dbWorkout);
-    }).catch(err => {
-        res.json(err);
-    });
+    db.Workout.aggregate([{
+        $addFields: {
+          totalDuration:{$sum: "$exercises.duration"}
+        }
+      }])
+        .sort({_id:-1}).limit(7)
+        .then(dbWorkout => {
+          res.json(dbWorkout);
+        })
+        .catch((err) => {
+          res.json(err);
+        });
 });
 
 module.exports = router
