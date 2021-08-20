25f721d8ce919c2627fefe42489a5c91d49e05e2
Karen Pion <karenlpion@gmail.com>
2021-08-20 01:14:52 -0400

    routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,6 +2,55 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 
+//Gets all workouts
+router.get("/api/workouts", (req, res) => {
+    db.Workout.find({}).then(dbWorkout => {
+        dbWorkout.forEach(workout => {
+            let total = 0;
+            workout.exercises.forEach(e => {
+                total += e.duration;
+            });
+            workout.totalDuration = total;
+        });
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+});
 
+// Add an exercise
+router.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+            $inc: { totalDuration: req.body.duration },
+            $push: { exercises: req.body }
+        },
+        { new: true }).then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+});
 
-module.exports = router;
+//Creates a workout
+router.post("/api/workouts", ({ body }, res) => {
+    console.log(body);
+    db.Workout.create(body).then((dbWorkout => {
+        res.json(dbWorkout);
+    })).catch(err => {
+        res.json(err);
+    });
+});
+
+// Get workouts in a range
+router.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({}).limit().
+    then(dbWorkout => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+});
+
+module.exports = router
--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
+app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
 
 app.listen(PORT,function(){ 
     console.log(`App listening on Port ${PORT}`);
